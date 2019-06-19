0566a8b804c7d666e2cc7b3320ef69941c4390da
AydovVitaly <voidmirror@users.noreply.github.com>
2019-06-19 08:51:06 +0400

    a bit html

---
+++ domCalculator/index.js
@@ -0,0 +1,8 @@
+const firstNumberElement = document.getElementById("firstNumber");
+const secondNumberElement = document.getElementById("secondNumber");
+
+let first = firstNumberElement.value || 0;
+let second = secondNumberElement.value || 0;
+
+first = parseInt(first, 10);
+second = parceInt(second, 10);
---
+++ domTwoButtons/index.js
@@ -0,0 +1,13 @@
+const leftButtonElement = document.getElementById('leftButton');
+const rightButtonElement = document.getElementById('rightButton');
+const textElement = document.getElementById("textBlock");
+
+function toRed() {
+    textElement.style.color = "#FF0000";
+}
+
+function toGreen() {
+    textElement.style.color = "#00CC00";
+}
+
+//leftButtonElement.addEventListener('click', )
